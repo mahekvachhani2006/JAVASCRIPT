235d20d34d2b55965ceb61bc27358f03c6eaad1a
mahek vachhani <mahekvachhani2006@gmail.com>
2025-07-31 15:25:27 +0530

    [mv][js repo create]

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let a =10;
+let b = "javascript";
+let i = "javascript";
+let c = true;
+let d = BigInt("1919");
+let e = null;
+let f = Symbol("js");
+let h = Symbol("js");
+let g ;
+console.log(a,b,c,d,e,f,g);
+console.log(e==i);
+console.log(f==h);
